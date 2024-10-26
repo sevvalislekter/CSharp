eb4195aa7816c132bce313f8a86bac564fff87f8
şevval işlekter <146951024+sevvalislekter@users.noreply.github.com>
2024-10-26 16:51:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3  basamaklı sayı girin");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            int hundred,tens,ones;
+            int sum;
+            ones = a % 10;
+            tens = (a % 100) / 10;
+            hundred = a / 100;
+
+            Console.WriteLine(ones + "-"+tens + "-"+hundred);
+            sum = ones + tens + hundred;
+            Console.WriteLine("3 basamaklı sayının toplamı " + sum);
+
+         }
+    }
+}
