480a9bf2efbcd3245de718226ea2cdf37232a937
şevval işlekter <146951024+sevvalislekter@users.noreply.github.com>
2024-10-28 18:54:27 +0300

    Add files via upload

---
+++ void-ornek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+          
+                string Country(string cName, string capital, string flagc)
+                {
+                    string card = "ülke" + cName + "başkent" + capital + "bayrak" + flagc;
+                    return card;
+                }
+                string x, y, z;
+                Console.WriteLine("ülke adını gir");
+                x = Console.ReadLine();
+                Console.WriteLine("baskenti gir");
+                y = Console.ReadLine();
+                Console.WriteLine("bayrak rengi gir");
+                z = Console.ReadLine();
+                Console.WriteLine(Country(x, y, z));
+
+            }
+        }
+    }
