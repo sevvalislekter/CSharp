9c61229be4ab33fb9c0a5c0258ee0e174f1c09bf
şevval işlekter <146951024+sevvalislekter@users.noreply.github.com>
2024-10-28 18:35:47 +0300

    Add files via upload

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("öğrenci sınav sistemi");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Sınıfta kaç öğrenci var");
+            int stdC = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------");
+
+            string[] isim = new string[stdC];
+            double[] exams = new double[stdC];
+
+            for (int i = 0; i < stdC; i++)
+            {
+                Console.WriteLine($"{i + 1} öğrencinin ismini yazın");
+                isim[i] = Console.ReadLine();
+                double total = 0;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"{isim[i]} isimli öğrencinin {j+1} sınav notunu girin");
+                     double value= double.Parse(Console.ReadLine());
+
+                    total += value;
+                }
+                exams[i] = total / 3;
+                
+            }
+            for (int i = 0; i < stdC; i++)
+            {
+                Console.WriteLine($"{isim[i]} öğrencinin ortalaması:{exams[i]}");
+
+            }
+            for (int i = 0; i < stdC; i++)
+            {
+                if (exams[i] >= 50)
+                {
+                    Console.WriteLine($"{isim[i]} adlı öğrenci dersi geçti");
+                }
+                else
+                {
+                    Console.WriteLine($"{isim[i]} adlı öğrenci dersi geçemedi");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp31/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ConsoleApp31")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp31")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("e9f74515-6bbf-47a8-895a-d31a30f67a04")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
